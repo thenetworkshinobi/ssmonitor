4ea17a895bfe1c2cae2b6154e4788e3aaad455c2
thenetworkshinobi <poetic.politik@gmail.com>
2025-03-27 15:33:22 -0400

    Update variables.php

--- config/variables.php
+++ config/variables.php
@@ -1,6 +1,10 @@
 <?php 
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
     #$host = "http://192.168.100.232/SSMonitor-New";
+=======
+    //$host = "http://192.168.100.232/SSMonitor-New";
+>>>>>>> Stashed changes
 =======
     //$host = "http://192.168.100.232/SSMonitor-New";
 >>>>>>> Stashed changes
