3a4e5b4e61a465f7115bcb47248c170d44b6d42a
thenetworkshinobi <poetic.politik@gmail.com>
2025-03-27 15:21:22 -0400

    test

--- config/db-connect.php
+++ config/db-connect.php
@@ -3,9 +3,9 @@
 
 class dbConnect
 {
-    private $dbhost = "192.168.100.232";
-    //private $dbhost = "td-jgriffith-l";
-    private $dbname = "sdash2";
+    //private $dbhost = "192.168.100.232";
+    private $dbhost = "td-jgriffith-l";
+    private $dbname = "ssmonitor";
     private $dbuser = "ssadminuser";
     private $dbpassword = "Password1";
     public $dbh = null;
