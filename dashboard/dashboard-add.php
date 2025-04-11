10e597e1f3f6269e385fefb65b6230c45116f301
thenetworkshinobi <poetic.politik@gmail.com>
2025-04-11 18:28:17 -0400

    ?

--- dashboard/dashboard-add.php
+++ dashboard/dashboard-add.php
@@ -2,7 +2,7 @@
     require_once "../config/variables.php";
     require_once "../partials/header.php";
     require_once "partials/login-verify.php";
-    include_once '../config/db-connect.php';
+    include_once "../config/db-connect.php";
     
 ?>
 
