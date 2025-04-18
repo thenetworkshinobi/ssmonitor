2f8888a31a6a360bb391c39cb05cc5c51a80e0c8
thenetworkshinobi <poetic.politik@gmail.com>
2025-04-18 11:51:23 -0400

    auto refresh snmp data
    change code to auto update snmp data now working

---
+++ dashboard/getdevicedata.php
@@ -0,0 +1,24 @@
+<?php
+header('Content-Type: application/json'); // Set the content type to JSON
+require_once "partials/json-handler.php";
+
+
+
+if (isset($_GET['ip_address'])) {
+    $jsonUrl = 'http://dragon-zord/ssmonitor/received_data.json'; // Replace with the actual JSON URL
+    $searchIp = htmlspecialchars($_GET['ip_address']);
+
+    // Fetch device data
+    $deviceData = getDeviceDataFromWeb($jsonUrl, $searchIp) ?? [];
+
+    // Return data as JSON
+    echo json_encode([
+        'cpu_usage' => $deviceData["cpu_usage"] ?? "unavailable",
+        'ram_usage_percentage' => $deviceData["ram_usage_percentage"] ?? "unavailable",
+        'network_throughput' => $deviceData["network_throughput"] ?? "unavailable",
+    ]);
+    exit;
+} else {
+    echo json_encode(['error' => 'No IP address provided']);
+}
+
--- dashboard/partials/json-handler.php
+++ dashboard/partials/json-handler.php
@@ -33,7 +33,7 @@ function getDeviceDataFromWeb($url, $searchIp) {
     return ["error" => "No device found with IP address: $searchIp"];
 }
 // Example usage
-$jsonUrl = "http://dragon-zord/ssmonitor/received_data.json"; // Path to the JSON file
+#$jsonUrl = "http://dragon-zord/ssmonitor/received_data.json"; // Path to the JSON file
 #$searchIp = "192.168.100.245";         // IP address to search for
 
 ?>
